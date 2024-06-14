2fc2615a6ed3fb4aeab1aeb88ef8d617a23b19bd
VirajTank <94729739+VirajTank@users.noreply.github.com>
2024-06-14 17:03:08 +0530

    Update monitor.sv

--- monitor.sv
+++ monitor.sv
@@ -2,7 +2,10 @@
 class alu_monitor extends uvm_monitor;
   //declare macros which are usefull
   `uvm_component_utils(alu_monitor)
+  alu_sequence_item item;
+  virtual alu_interface vif;
   
+  uvm_analysis_port #(alu_sequence_item) monitor_port;
   
   //write a contructor 
   function new(string name = "alu_monitor", uvm_component parent);
@@ -20,6 +23,13 @@ class alu_monitor extends uvm_monitor;
     super.build_phase(phase);
     `uvm_info("MONITOR_CLASS", "Build Phase", UVM_HIGH)
     
+    monitor_port = new("monitor_port",this);
+
+    if(!(uvm_config_db #(virtual alu_interface)::get(this,"*","vif",vif))) begin
+      `uvm_error("MONITOR_CLASS","Failed to get vif from config_db")
+    end
+    
+    
   endfunction: build_phase
   
   //connect phase
@@ -34,9 +44,29 @@ class alu_monitor extends uvm_monitor;
   //functions cannot include any time consuming statements
   task run_phase(uvm_phase phase);
     super.run_phase(phase);
-    
+    `uvm_info("MONITOR_CLASS","Inside Run Phase", UVM_HIGH)
     //logic
-    
+    forever begin
+      item = alu_sequence_item::type_id::create("item");
+      
+      wait(!vif.reset);
+      
+      //sample the inputs
+      @(posedge vif.clock);
+      item.a = vif.a;
+      item.b = vif.b;
+      item.op_code = vif.op_code;
+      
+      //sample the outputs
+      @(posedge vif.clock);
+      item.result = vif.result;
+      
+      
+      //send item to scoreboard
+      monitor_port.write(item);
+      
+    end
+        
   endtask: run_phase
   
 endclass: alu_monitor
