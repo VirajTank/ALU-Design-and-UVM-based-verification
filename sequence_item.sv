26c41e0c48a0aeeb22ac6b6910c41614bfa0834d
VirajTank <94729739+VirajTank@users.noreply.github.com>
2024-06-14 17:03:43 +0530

    Update sequence_item.sv

--- sequence_item.sv
+++ sequence_item.sv
@@ -15,6 +15,12 @@ class alu_sequence_item extends uvm_sequence_item;
   logic [7:0] result;
   bit carry_out;
   
+  //default contraints
+  constraint input1_c {a inside {[10:20]};}
+  constraint input2_c {b inside {[01:10]};}
+  constraint op_code_c {op_code inside {0,1,2,3};}
+  
+  
   function new(string name = "alu_sequence_item");
     super.new(name);
     
