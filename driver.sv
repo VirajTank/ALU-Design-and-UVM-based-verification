dd5f8f6c995340c85b22ac5ecece71574ce987d5
VirajTank <94729739+VirajTank@users.noreply.github.com>
2024-06-14 17:02:05 +0530

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -3,6 +3,8 @@ class alu_driver extends uvm_driver#(alu_sequence_item);
   //declare macros which are usefull
   `uvm_component_utils(alu_driver)
   
+  virtual alu_interface vif;
+  alu_sequence_item item;
   
   //write a contructor 
   function new(string name = "alu_driver", uvm_component parent);
@@ -20,6 +22,10 @@ class alu_driver extends uvm_driver#(alu_sequence_item);
     super.build_phase(phase);
     `uvm_info("DRIVER_CLASS", "Build Phase", UVM_HIGH)
     
+    if(!(uvm_config_db #(virtual alu_interface)::get(this,"*","vif",vif))) begin
+      `uvm_error("DRIVER_CLASS","Failed to get vif from config_db")
+    end
+    
   endfunction: build_phase
   
   //connect phase
@@ -34,10 +40,28 @@ class alu_driver extends uvm_driver#(alu_sequence_item);
   //functions cannot include any time consuming statements
   task run_phase(uvm_phase phase);
     super.run_phase(phase);
+    `uvm_info("DRIVER_CLASS","inside run phase", UVM_HIGH)
     
-    //logic
+    forever begin
+      item = alu_sequence_item::type_id::create("item");
+      seq_item_port.get_next_item(item);
+      //logic
+      drive(item);
+      seq_item_port.item_done();
+      
+    end
     
   endtask: run_phase
+  
+  //drive task
+  task drive(alu_sequence_item item);
+    @(posedge vif.clock)
+    vif.reset <= item.reset;
+    vif.a <= item.a;
+    vif.b <= item.b;
+    vif.op_code <= item.op_code;
+    
+  endtask:drive
   
 endclass: alu_driver
  
